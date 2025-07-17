1075b2cc1658d9d1bb82696d945e79a687541447
Piotr Patek <piotrpatek17@gmail.com>
2025-07-17 23:24:07 +0200

    Removed unused imports

--- elkapod_visualization/launch/display.launch.py
+++ elkapod_visualization/launch/display.launch.py
@@ -1,18 +1,10 @@
 import os
 from launch import LaunchDescription
-from launch.substitutions import Command, LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch_ros.parameter_descriptions import ParameterValue
-from launch.conditions import IfCondition
-import yaml
+
 
 def generate_launch_description():
-    urdf_launch_package = FindPackageShare(package="elkapod_description").find(
-        "elkapod_description"
-    )
-
     vis_package = FindPackageShare(package="elkapod_visualization").find(
         "elkapod_visualization"
     )
