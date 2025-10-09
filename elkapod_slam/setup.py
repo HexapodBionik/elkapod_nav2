f2be15765f85486fdb12b9f87881f099da140bc0
Damian <baraniak.damian@gmail.com>
2025-10-09 10:25:30 +0200

    init

---
+++ elkapod_slam/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'elkapod_slam'
+data_files = []
+data_files.append(
+    ('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append(('share/' + package_name, ['package.xml']))
+data_files.append((os.path.join('share', package_name, 'launch'), glob(
+    os.path.join('launch', '*launch.[pxy][yma]*'))))
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=data_files,
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='root@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
